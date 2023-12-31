e77e0ebaf2f8a67c15897243fa1f63298c461e87
Adnan Tariq <adnan6official@gmail.com>
2023-12-31 23:57:08 +0500

    created navbar and footer section

---
+++ index.js
@@ -0,0 +1,24 @@
+const navbar = document.querySelector(".navbar");
+window.addEventListener("scroll", () => {
+  window.scrollY > 100
+    ? navbar.classList.add("nav-scroll")
+    : navbar.classList.remove("nav-scroll");
+});
+
+// SWIPER JS
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
